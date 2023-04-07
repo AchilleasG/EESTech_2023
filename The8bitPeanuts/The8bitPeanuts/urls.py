f818773a9971be00ab233161754919db5d610338
ageorgiou <ageorgiou@cognity.gr>
2023-04-08 01:45:41 +0300

    Make basic profile front_end and mood tracker on back_end

--- The8bitPeanuts/The8bitPeanuts/urls.py
+++ The8bitPeanuts/The8bitPeanuts/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import  path
+from django.conf import settings
 from habit_manager import views as v_hm
-
+from user_system import views as v_us
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('h_m/', v_hm.index),
-]
+    path('u/<str:username>', v_us.user_profile)
+]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- The8bitPeanuts/user_system/models.py
+++ The8bitPeanuts/user_system/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 
 # Create your models here.
 class Basic_User(models.Model):
@@ -8,9 +8,23 @@ class Basic_User(models.Model):
     password = models.CharField(max_length=20)
 class User (Basic_User):
     total_points = models.IntegerField()
+    icon= models.ForeignKey('Icon', on_delete=models.CASCADE, default=1)
+    achievement = models.ManyToManyField('Achievement',through='UserAchievement', blank=True)
+    joined_date = models.DateField(default=datetime.date.today)
+class Achievement(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    icon = models.ImageField(upload_to='media/achievement_icons')
 
+class UserAchievement(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+class Day (models.Model):
+    date =models.DateField(default=datetime.date.today)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    mood = models.IntegerField()
 class Therapist(Basic_User):
-    user = models.ManyToManyField(User,through='UserTherapist')
+    user = models.ManyToManyField(User,through='UserTherapist', blank=True)
 
 class UserTherapist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -18,5 +32,5 @@ class UserTherapist(models.Model):
     has_access = models.BooleanField(default=False)
 class Icon(models.Model):
     points_required = models.IntegerField(default=0)
-    image = models.ImageField(upload_to='media')
+    image = models.ImageField(upload_to='media/user_icons')
 
